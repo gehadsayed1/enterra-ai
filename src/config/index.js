564a91f0a315f713e7a501036fa30c8d0cc586cc
jihad <gehadsaed199@gmail.com>
2025-11-25 15:25:33 +0200

    feat: add initial configuration file with API and WebSocket URLs

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
   API_BASE_URL: "https://qwm785tg5osg9p-8888.proxy.runpod.net",
-  WEBSOCKET_URL: "ws://qwm785tg5osg9p-8888.proxy.runpod.net/ws/voice",
+  WEBSOCKET_URL: "wss://qwm785tg5osg9p-8888.proxy.runpod.net/ws/voice",
 };
